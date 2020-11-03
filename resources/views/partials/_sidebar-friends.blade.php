b425b99580502af5a2da6d9888aa665af3220969
krendzel <krendzel@gmail.com>
2020-11-03 16:35:56 +0100

    remove static list followings users

--- resources/views/partials/_sidebar-friends.blade.php
+++ resources/views/partials/_sidebar-friends.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-gray-200 p-4 rounded-lg">
-    <h3 class="font-semibold text-lg mb-4">Friends</h3>
+    <h3 class="font-semibold text-lg mb-4">Following</h3>
     <ul>
-        @foreach(range(1,8) as $index )
+        @foreach(auth()->user()->follows as $user)
         <li class="hover:bg-gray-300 px-4 py-2 rounded-lg mb-2">
             <div class="flex items-center text-sm">
-                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
-                <span class="pl-4 ">John Doe</span>
+                <img src="{{ $user->avatar }}" alt="" class="rounded-full">
+                <span class="pl-4 ">{{ $user->name }}</span>
             </div>
         </li>
         @endforeach
