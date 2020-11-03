f737828d88110b64ce43d273c9dc8b44ced27571
krendzel <krendzel@gmail.com>
2020-11-03 16:02:19 +0100

    Delete static text

--- resources/views/partials/_timeline.blade.php
+++ resources/views/partials/_timeline.blade.php
@@ -1,14 +1,14 @@
 <div class="tweet-item flex border-b mb-4 py-6 px-4">
     <div class="flex-shrink-0 mr-4">
-        <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+        <img src="{{ auth()->user()->avatar }}" alt="" class="rounded-full">
     </div>
     <div>
         <div class="flex items-center">
-            <h3 class="font-semibold pr-4">John Doe</h3>
-            <span class="text-sm">1 minute ago</span>
+            <h3 class="font-semibold pr-4">{{ $tweet->user->name }}</h3>
+            <span class="text-sm">{{ $tweet->created_at }}</span>
         </div>
         <div class="mt-2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur earum facilis optio quis! Deleniti error exercitationem maxime minus possimus qui vitae! Animi asperiores deserunt eaque inventore natus, quam quia sint!</p>
+            <p>{{ $tweet->body }}</p>
         </div>
         <div class="py-4">
             <a href="#" class="px-3 py-2 rounded-md text-xs font-medium text-indigo-500 bg-indigo-200">+24</a>
