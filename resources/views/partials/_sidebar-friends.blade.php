6ad66d9d0a2d5a200f1599aab0167c8708ccde9e
krendzel <krendzel@gmail.com>
2020-11-03 19:50:56 +0100

    Fix avatar size

--- resources/views/partials/_sidebar-friends.blade.php
+++ resources/views/partials/_sidebar-friends.blade.php
@@ -2,11 +2,13 @@
     <h3 class="font-semibold text-lg mb-4">Following</h3>
     <ul>
         @foreach(auth()->user()->follows as $user)
-        <li class="hover:bg-gray-300 px-4 py-2 rounded-lg mb-2">
+        <li class="hover:bg-gray-300 px-4 py-2 rounded-lg mb-2 hover:text-indigo-600">
+            <a href="{{ route('profile.show', $user->name) }}">
             <div class="flex items-center text-sm">
-                <img src="{{ $user->avatar }}" alt="" class="rounded-full">
-                <span class="pl-4 ">{{ $user->name }}</span>
+                <img src="{{ $user->avatar }}" alt="" class="rounded-full" style="width: 40px">
+                <span class="pl-4">{{ $user->name }}</span>
             </div>
+            </a>
         </li>
         @endforeach
     </ul>
