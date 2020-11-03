5d4cb0383f59dd02a8765df23d6e805f8efd58c3
krendzel <krendzel@gmail.com>
2020-11-03 15:12:55 +0100

    Create tweet panel view

---
+++ resources/views/partials/_tweet-panel.blade.php
@@ -0,0 +1,10 @@
+<div class="border-2 rounded-lg border-indigo-200 p-4">
+    <form action="#">
+        <textarea name="tweet_msg" id="" placeholder="Tweet something" class="w-full text-lg p-2  focus:outline-none"></textarea>
+        <hr class="my-4">
+        <div class="flex justify-between p-2">
+            <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+            <a href="#" class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-full hover:bg-indigo-600">Tweet a-roo!</a>
+        </div>
+    </form>
+</div>
