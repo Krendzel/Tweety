9acaeea99404068011fbb01c0bb47fa70374448d
krendzel <krendzel@gmail.com>
2020-11-03 19:51:59 +0100

    Add dynamic link to profile

--- resources/views/partials/_timeline.blade.php
+++ resources/views/partials/_timeline.blade.php
@@ -1,11 +1,11 @@
 <div class="tweet-item flex border-b mb-4 py-6 px-4">
     <div class="flex-shrink-0 mr-4">
-        <img src="{{ auth()->user()->avatar }}" alt="" class="rounded-full">
+        <img src="{{ auth()->user()->avatar }}" alt="" class="rounded-full" style="width: 40px">
     </div>
     <div>
         <div class="flex items-center">
-            <h3 class="font-semibold pr-4">{{ $tweet->user->name }}</h3>
-            <span class="text-sm">{{ $tweet->created_at }}</span>
+            <h3 class="font-semibold pr-4 hover:text-indigo-600 hover:underline"><a href="{{ route('profile.show', $tweet->user->name) }}">{{ $tweet->user->name }}</a></h3>
+            <span class="text-sm">{{ $tweet->created_at->diffForHumans() }}</span>
         </div>
         <div class="mt-2">
             <p>{{ $tweet->body }}</p>
