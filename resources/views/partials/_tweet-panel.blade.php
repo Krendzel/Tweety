58056a7ce29d9129c6686e0a3765c6662cf2922f
krendzel <krendzel@gmail.com>
2020-11-03 16:03:01 +0100

    Delete static text and add request validation functionality

--- resources/views/partials/_tweet-panel.blade.php
+++ resources/views/partials/_tweet-panel.blade.php
@@ -1,10 +1,14 @@
 <div class="border-2 rounded-lg border-indigo-200 p-4">
-    <form action="#">
-        <textarea name="tweet_msg" id="" placeholder="Tweet something" class="w-full text-lg p-2  focus:outline-none"></textarea>
+    <form action="/tweets" method="POST">
+        @csrf
+        <textarea name="body" id="" placeholder="Tweet something" class="w-full text-lg p-2  focus:outline-none" required></textarea>
+        @error('body')
+        <span class="text-sm text-red-500">{{ $message }}</span>
+        @enderror
         <hr class="my-4">
         <div class="flex justify-between p-2">
-            <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
-            <a href="#" class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-full hover:bg-indigo-600">Tweet a-roo!</a>
+            <img src="{{ auth()->user()->avatar }}" alt="" class="rounded-full">
+            <button href="#" class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-full hover:bg-indigo-600">Tweet a-roo!</button>
         </div>
     </form>
 </div>
