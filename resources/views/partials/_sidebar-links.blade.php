504546ac33467ee563fe7464873983b284fffda4
krendzel <krendzel@gmail.com>
2020-11-03 19:51:10 +0100

    Add dynamic links to menu

--- resources/views/partials/_sidebar-links.blade.php
+++ resources/views/partials/_sidebar-links.blade.php
@@ -1,5 +1,5 @@
 <ul>
-    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Home</a></li>
+    <li class="pb-2"><a href="{{ route('tweets') }} " class="{{ request()->is('tweets') ? 'bg-gray-200 text-indigo-600' : '' }} block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Home</a></li>
     <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Explore</a></li>
     <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Notifications</a></li>
     <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Messages</a></li>
