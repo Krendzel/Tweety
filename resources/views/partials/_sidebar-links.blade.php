21b44887e2b06f2aa52c090a8f8fa5d53056f483
krendzel <krendzel@gmail.com>
2020-11-03 15:11:51 +0100

    remove class w-1/2

--- resources/views/partials/_sidebar-links.blade.php
+++ resources/views/partials/_sidebar-links.blade.php
@@ -1,12 +1,12 @@
 <ul>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Home</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Explore</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Notifications</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Messages</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Bookmarks</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Lists</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Profile</a></li>
-    <li class="pb-2"><a href="#" class="block w-1/2 px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">More</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Home</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Explore</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Notifications</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Messages</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Bookmarks</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Lists</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">Profile</a></li>
+    <li class="pb-2"><a href="#" class="block px-3 py-2 rounded-md text-md font-medium hover:bg-gray-200">More</a></li>
 </ul>
 <div class="p-4">
     <a href="#" class="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-full hover:bg-indigo-600">Tweet a-roo!</a>
