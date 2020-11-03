4b1dc8a9d9d5da5eaf22bf23d94a525dbb9b6219
krendzel <krendzel@gmail.com>
2020-11-03 14:08:44 +0100

    Add sidebar friends

---
+++ resources/views/partials/_sidebar-friends.blade.php
@@ -0,0 +1,35 @@
+<div class="bg-gray-200 p-4 rounded-lg">
+    <h3 class="font-semibold text-lg mb-4">Friends</h3>
+    <ul>
+        <li class="hover:bg-gray-300 p-2 rounded-lg mb-2">
+            <div class="flex items-center">
+                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+                <span class="pl-4 ">John Doe</span>
+            </div>
+        </li>
+        <li class="hover:bg-gray-300 p-2 rounded-lg mb-2">
+            <div class="flex items-center">
+                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+                <span class="pl-4 ">John Doe</span>
+            </div>
+        </li>
+        <li class="hover:bg-gray-300 p-2 rounded-lg mb-2">
+            <div class="flex items-center">
+                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+                <span class="pl-4 ">John Doe</span>
+            </div>
+        </li>
+        <li class="hover:bg-gray-300 p-2 rounded-lg mb-2">
+            <div class="flex items-center">
+                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+                <span class="pl-4 ">John Doe</span>
+            </div>
+        </li>
+        <li class="hover:bg-gray-300 p-2 rounded-lg mb-2">
+            <div class="flex items-center">
+                <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+                <span class="pl-4 ">John Doe</span>
+            </div>
+        </li>
+    </ul>
+</div>
