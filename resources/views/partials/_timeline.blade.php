9fc12f3da1fe99fc9cdbbb2021d3836ce1dfcc7e
krendzel <krendzel@gmail.com>
2020-11-03 15:12:17 +0100

    Create timeline view

---
+++ resources/views/partials/_timeline.blade.php
@@ -0,0 +1,18 @@
+<div class="tweet-item flex border-b mb-4 py-6 px-4">
+    <div class="flex-shrink-0 mr-4">
+        <img src="https://i.pravatar.cc/40?img=3" alt="" class="rounded-full">
+    </div>
+    <div>
+        <div class="flex items-center">
+            <h3 class="font-semibold pr-4">John Doe</h3>
+            <span class="text-sm">1 minute ago</span>
+        </div>
+        <div class="mt-2">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur earum facilis optio quis! Deleniti error exercitationem maxime minus possimus qui vitae! Animi asperiores deserunt eaque inventore natus, quam quia sint!</p>
+        </div>
+        <div class="py-4">
+            <a href="#" class="px-3 py-2 rounded-md text-xs font-medium text-indigo-500 bg-indigo-200">+24</a>
+            <a href="#" class="px-3 py-2 rounded-md text-xs font-medium hover:text-indigo-500 hover:bg-indigo-100">+24</a>
+        </div>
+    </div>
+</div>
